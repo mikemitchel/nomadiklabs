1cd3ca5257a738ed1b0d891bff4e4e8d26598d4c
mitch <mitch@nomadiklabs.com>
2025-09-24 14:56:02 -0400

    convert to sveltekit

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ dark blue -> #172830
 text yellow -> #fff9d1;
 orange -> #ff8100
 background => #fff4de
+
+#cc7722 Ochre
+#9f7b3e Brown Ochre
+#bb4f35 Burnt Ochre
+#c77943 Golden Ochre
+#9f8f55 Green Ochre
+#9b753d Heavy Ochre
+#cc7733 Ochre Maroon
+#efcc83 Yellow Ochre
+#cb9d06 Dark Yellow Ochre
+#837752 Olive Ochre
+#dc793a Orange Ochre
+#913832 Red Ochre
---
+++ src/routes/+page.svelte
@@ -0,0 +1,83 @@
+<script>
+	import logo from '$lib/assets/logo.svelte';
+</script>
+
+<div class="container">
+	<div class="box">
+		<svelte:component this={logo} />
+		<div class="name">NOMADIK LABS</div>
+	</div>
+	<div class="tag">All that is gold does not glitter,</div>
+	<div class="tag">Not all who wander are lost.</div>
+	<div class="tag">The old that is strong does not wither,</div>
+	<div class="tag">Deep roots are not reached by the frost.</div>
+	<br />
+	<div class="tag">~J.R.R Tolkien</div>
+</div>
+<footer>
+	<p class="copyright">© 2024-2025 Nomadik Labs LLC. All rights reserved</p>
+</footer>
+
+<style>
+	:global(body) {
+		background-image: url($lib/assets/sunset.jpeg);
+		background-position: center center;
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-size: cover;
+		background-color: #fff4de;
+
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		overflow: hidden;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.box {
+		margin: 10vh;
+		display: flex;
+		align-items: center;
+		flex-direction: row;
+	}
+
+	.name {
+		margin: 10px;
+		font-family: Tahoma, Arial, Helvetica, sans-serif;
+		font-weight: bold;
+		font-size: 6vmin;
+		color: #172830;
+	}
+
+	.tag {
+		display: flex;
+		justify-content: center;
+		margin: 10px;
+		font-family: Tahoma, Arial, Helvetica, sans-serif;
+		font-style: italic;
+		font-size: 3vmin;
+		color: #fff4de;
+	}
+
+	.copyright {
+		font-family: Tahoma, Arial, Helvetica, sans-serif;
+		color: #fff4de;
+	}
+
+	p.copyright {
+		margin-bottom: 15px;
+		font-size: 3vmin;
+	}
+
+	footer {
+		display: flex;
+		justify-content: center;
+		margin-top: auto;
+	}
+</style>
